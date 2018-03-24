e9edab7400cc9a514327fdf3f8f7c659d1e772d2
Marcus Jun <mjun@calstate.edu>
2018-03-24 21:26:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,7 @@
 def position_taken? (array,index)
   if (array[index]==" " || array[index]=="" || array[index]==nil)
     return false
+  elsif(array[index]=="X"||array[index]=="O")
+    return true
   end
 end
